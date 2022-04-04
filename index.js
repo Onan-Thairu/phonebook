2774cff90a825248d431ceb9e480c5990aa1fe87
Onan Thairu <thairuonan@gmail.com>
2022-04-04 11:52:17 +0300

    Integrate mongodb

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { request } = require("express")
+require('dotenv').config()
 const express = require("express")
 const { status } = require("express/lib/response")
 const app = express()
 const morgan = require("morgan")
 const cors = require("cors")
+const Person = require("./models/person")
 
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -12,61 +14,36 @@ app.use(cors())
 
 app.use(express.static('build'))
 
-let persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-app.get('/info', (request, response) => {
-    const noOfPple = persons.length
-    const time = new Date()
-    response.send(`
-    <p>Phonebook has info for ${noOfPple} people.</p>
-    <p>${time}</p>
-    `)
-})
+// app.get('/info', (request, response) => {
+//     const noOfPple = persons.length
+//     const time = new Date()
+//     response.send(`
+//     <p>Phonebook has info for ${noOfPple} people.</p>
+//     <p>${time}</p>
+//     `)
+// })
+
+
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if(person){
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).send('User not found')
-    }
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     persons = persons.filter(person => person.id !== id)
+//     response.status(204).end()
+// })
 
-const generateId = () => {
-    return Math.floor(Math.random()*500)
-}
 
 morgan.token('postLog', (req, res) => {
     return JSON.stringify(req.body)
@@ -77,27 +54,29 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 app.post('/api/persons/', (request, response) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
+    if (body.name === undefined || body.number === undefined){
         return response.status(400).json({error:'Content missing'})
-    } else if (persons.find(p => p.name === body.name )) {
-        return response.status(400).json({error:'Name must be unique'})
-    }
-
-    const person = {
-        id: generateId(),
+    } 
+    // else {
+    //     Person.find({}).then(p => {
+    //         if(p.name === body.name) {
+    //             return response.status(400).json({error:'Name must be unique'})
+    //         } 
+    //     })
+    // }
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
     
-    response.json(person)
 })
 
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password> <optional>')
+    process.exit(1)
+} else if (process.argv.length === 3) {
+    const password = process.argv[2]
+
+    const url = `mongodb+srv://fStack${password}@cluster0.7rmdo.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+    mongoose.connect(url)
+
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: Number,
+    })
+
+    const People = mongoose.model('People', personSchema)
+
+    People
+        .find({})
+        .then(result => {
+            result.forEach(person => console.log(person))
+            mongoose.connection.close()
+    })
+
+} else {
+    const password = process.argv[2]
+
+    const url = `mongodb+srv://fStack:${password}@cluster0.7rmdo.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+    
+    mongoose.connect(url)
+    
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: Number,
+    })
+    
+    const Person = mongoose.model('Person', personSchema)
+    
+    const argName = process.argv[3]
+    const argNumber = process.argv[4]
+    
+    const person = new Person({
+        name: argName,
+        number: argNumber,
+    })
+    
+    person.save().then(result => {
+        console.log(`Added ${argName} - Number: ${argNumber} to Phonebook`)
+        mongoose.connection.close()
+    })
+
+}
